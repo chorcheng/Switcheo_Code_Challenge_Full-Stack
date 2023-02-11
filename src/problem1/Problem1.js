443cc9ed70da5b6df472e1262a95e868af340401
chorcheng <chanchorcheng2340@gmail.com>
2023-02-11 17:02:26 +0800

    First commit

---
+++ src/problem1/Problem1.js
@@ -0,0 +1,25 @@
+var sum_to_n_a = function(n) {
+    // your code here
+    return (n*(n+1)/2);
+};
+
+var sum_to_n_b = function(n) {
+    // your code here
+    var sum = 0;
+    for (var i = 0; i < n+1; i++){
+        sum += i;
+    }
+    return sum;
+};
+
+var sum_to_n_c = function(n) {
+    // your code here
+    if (n == 1){
+        return n;
+    }
+    return sum_to_n_c(n-1) + n;
+};
+
+console.log("a" + sum_to_n_a(7));
+console.log("b" + sum_to_n_b(7));
+console.log("c" + sum_to_n_c(7));
